3b609165bce0d6399aadacf63fc9521cbcecfe74
wmartinezg <wilson_martinez_g@hotmail.com>
2018-02-20 09:45:58 -0500

    Usando MongoDB con Node-GraphQL

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
+import mongoose from 'mongoose';
+mongoose.Promise = global.Promise;
 
 import typeDefs from './schemas'
 import resolvers from './resolvers'
+import models from './models' // seria lo mismo que './models/index.js', pero toma el index quitandolo
 
 const schema = makeExecutableSchema({
     typeDefs,
@@ -15,9 +18,22 @@ const PORT = 3000;
 const app = express();
 
 // bodyParser is needed just for POST.
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+    schema,
+    context: {
+        models
+    }
+}));
 app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' })); // if you want GraphiQL enabled
 
-app.listen(PORT, () => {
-    console.log('Running GRAPHQL server...');
-});
+//conectar a mongo una bd
+mongoose.connect('mongodb://localhost:27017/instagram-clone').then(
+    () => {
+        console.log('Conectado a Mongo!!!')
+        app.listen(PORT, () => {
+            console.log('Running GRAPHQL server...'); //una vez conecte a mongo se
+                                                      //corre express
+        });
+    }
+)
+
--- schemas.js
+++ schemas.js
@@ -1,5 +1,16 @@
 export default `
-    type Query{
-        holaMundo: String
+type User{
+    _id: ID!
+    username: String!
+    password: String!
+}    
+
+type Query{
+        allUsers: [User]!
+        getUser(_id: ID!): User!
+    }
+
+    type Mutation{
+        createUser(username: String!, password: String!): User
     }
 `;
